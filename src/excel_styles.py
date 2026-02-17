c5af32914d93df9e5e0252b6bc2de4366f8c750b
NeethirajahAjey <neethirajahajey2002@gmail.com>
2026-02-17 12:00:19 +0530

    feat: implemented custom excel templates

---
+++ src/excel_styles.py
@@ -0,0 +1,25 @@
+from openpyxl.styles import Font, Alignment, PatternFill, Border, Side
+
+
+def setup_table_styles():
+    header_fill = PatternFill("solid", fgColor="BDD7EE")  # light blue (closer to screenshot)
+    header_font = Font(color="000000", bold=True)
+    header_align = Alignment(horizontal="center", vertical="center", wrap_text=True)
+
+    title_font = Font(bold=True, size=14)
+    title_align = Alignment(horizontal="center", vertical="center")
+
+    thin = Side(style="thin", color="000000")
+    border = Border(left=thin, right=thin, top=thin, bottom=thin)
+
+    total_font = Font(bold=True)
+
+    return {
+        "header_fill": header_fill,
+        "header_font": header_font,
+        "header_align": header_align,
+        "title_font": title_font,
+        "title_align": title_align,
+        "border": border,
+        "total_font": total_font,
+    }
